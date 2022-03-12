afa8c0ebe69d5bf5046f0c7ffc11badc6cda3ff8
mndrs <mndrs.kc@gmail.com>
2022-03-12 18:35:33 -0500

    test connection to the database

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//Express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//Connect to database
+// Connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -19,13 +19,14 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-//Default response for any other request (Not Found)
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
