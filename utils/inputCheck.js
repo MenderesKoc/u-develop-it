9e0c849ffd4edd72a8bb77a5dc48ba78ab820fbb
mndrs <mndrs.kc@gmail.com>
2022-03-12 20:05:39 -0500

    created new table

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,4 +1,7 @@
-module.exports = function (obj, ...props) {
+// utility to check if an object has the required properties
+// e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
+
+module.exports = function(obj, ...props) {
   const errors = [];
 
   props.forEach(prop => {
