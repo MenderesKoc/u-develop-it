b6e9dc12fcca019defabe01f3d647ec6984f1db9
mndrs <mndrs.kc@gmail.com>
2022-03-13 19:31:05 -0400

    added voter routes

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,10 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-// Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root61',
-    database: 'election'
+  host: 'localhost',
+  user: 'root',
+  password: 'root61',
+  database: 'election'
 });
-
 
 module.exports = db;
